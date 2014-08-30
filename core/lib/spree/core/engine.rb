8ac20ffefc2431c507f10c9548c3fabb7006fc60
Briege Murray <briege.murray@examtime.com>
2014-08-30 13:34:28 +0100

    See #1461 - getting asset precompilation error appears to originate in spree - intend to test out some changes to fork of spree in attempt to address this

--- core/lib/spree/core/engine.rb
+++ core/lib/spree/core/engine.rb
@@ -83,7 +83,7 @@ module Spree
       # sets the manifests / assets to be precompiled, even when initialize_on_precompile is false
       initializer "spree.assets.precompile", :group => :all do |app|
         app.config.assets.precompile += %w[
-          admin/globals/*
+          admin/globals/all.css
           store/all.*
           admin/all.*
           admin/orders/edit_form.js
